b2754459b6d3a8abf59714e64564a27937b227f7
Prajna1999 <32980302+Prajna1999@users.noreply.github.com>
2023-05-05 19:27:33 +0000

    fix: user session bugs

--- test2.js
+++ test2.js
@@ -1,95 +1,19 @@
-const readline = require("readline");
+const { createClient } = require('@supabase/supabase-js')
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const SUPABASE_URL = "https://roxbbbxlqouzumqfsstv.supabase.co"
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJveGJiYnhscW91enVtcWZzc3R2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMwMDU1MTAsImV4cCI6MTk5ODU4MTUxMH0.rhEzoIV-9UB8me5RXag7mOsitHWLKSM53UHwk97YddQ"
 
-const users = [];
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
 
-function login() {
-  rl.question("Please enter your username: ", (username) => {
-    rl.question("Please enter your password: ", (password) => {
-      const user = users.find(
-        (user) => user.username === username && user.password === password
-      );
 
-      if (user) {
-        console.log(`Welcome back, ${username}!\n`);
-        promptMainMenu(); // Call to the main menu function after successful login
-      } else {
-        console.log("Invalid login information. Please try again.\n");
-        login();
-      }
-    });
-  });
-}
-
-function signup() {
-  rl.question("Please choose a username: ", (username) => {
-    rl.question("Please choose a password: ", (password) => {
-      users.push({ username, password });
-      console.log("Signup successful. Please log in.\n");
-      login();
-    });
-  });
-}
-
-function promptMainMenu() {
-  console.log("\n");
-  console.log("MAIN MENU");
-  console.log("1. Post an item");
-  console.log("2. Comment on a post");
-  console.log("3. Reply to a comment");
-  console.log("4. Follow a user");
-  console.log("5. Unfollow a user");
-  console.log("6. Upvote a post");
-  console.log("7. Downvote a post");
-  console.log("8. Upvote a comment");
-  console.log("9. Logout");
-  console.log("\n");
-
-  rl.question("Please enter your choice: ", (choice) => {
-    switch (choice) {
-      case "1":
-        console.log("You selected 'Post an item'");
-        break;
-      case "2":
-        console.log("You selected 'Comment on a post'");
-        break;
-      case "3":
-        console.log("You selected 'Reply to a comment'");
-        break;
-      case "4":
-        console.log("You selected 'Follow a user'");
-        break;
-      case "5":
-        console.log("You selected 'Unfollow a user'");
-        break;
-      case "6":
-        console.log("You selected 'Upvote a post'");
-        break;
-      case "7":
-        console.log("You selected 'Downvote a post'");
-        break;
-      case "8":
-        console.log("You selected 'Upvote a comment'");
-        break;
-      case "9":
-        console.log("You selected 'Logout'");
-        break;
-      default:
-        console.log("Invalid choice. Please try again.\n");
-        promptMainMenu();
-    }
-  });
-}
-
-console.log("Welcome to my application!");
-rl.question("Do you have an account? (y/n): ", (answer) => {
-  if (answer === "y") {
-    login();
-  } else {
-    signup();
+async function getUsers() {
+  const { data, error } = await supabase.from('users').select('*')
+  if (error) {
+    console.log(error)
+    return
   }
-});
+  console.log(data)
+}
+
+
+getUsers()
